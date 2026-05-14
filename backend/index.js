ea6fdeef97fbe0bc0d719252bac6c237bd4a49a5
Aulia <aulianhrn@gmail.com>
2026-05-14 14:09:26 +0700

    update file

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Backend aman mas 😎☝️");
+});
+
 app.use("/api", noteRoutes);
 
 const PORT = process.env.PORT || 8080;
